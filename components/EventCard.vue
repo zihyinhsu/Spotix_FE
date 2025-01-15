57d4127dabf46f153b3d05941c3d550fec31bb4f
Zihyin <jenny010328@gmail.com>
2025-01-15 10:57:00 +0800

    style : eventCard

--- components/EventCard.vue
+++ components/EventCard.vue
@@ -9,7 +9,7 @@ const props = defineProps<{
 <template>
   <div class="w-full h-full rounded-md overflow-hidden hover:shadow-2xl hover:-translate-y-1 transition-ease cursor-pointer hover-primary flex flex-col">
     <img
-      class="object-cover w-full max-h-[160px] md:max-h-[80px]"
+      class="object-top object-cover w-full max-h-[160px] md:max-h-[90px]"
       :src="props.event.coverUrl"
       alt="event"
     >
