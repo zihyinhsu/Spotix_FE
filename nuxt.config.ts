e70915e59e37d6daf6f67ec49be379c269d6de7a
Zihyin <jenny010328@gmail.com>
2025-01-15 14:19:25 +0800

    change : apiBase/0105

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -71,7 +71,7 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      apiBase: 'https://8894-114-34-72-78.ngrok-free.app/',
+      apiBase: 'https://766a-114-34-72-78.ngrok-free.app/',
     },
     newebpay: {
       MerchantID: process.env.MerchantID,
